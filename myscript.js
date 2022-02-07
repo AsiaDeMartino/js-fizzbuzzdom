e0deb291d4ff2586ee2a3b76fe340b46053e24ec
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-07 23:27:36 +0100

    creato titolo e struttura base js

---
+++ myscript.js
@@ -0,0 +1,5 @@
+// collegare variabile ad un contenitore in html
+//creare loop che stampa numeri da 1 a 100
+    //selezionare i numeri che sono sia multipli di 3 che multipli di 5
+        //selezionare multipli di 3
+        //selezionare multipli di 5
