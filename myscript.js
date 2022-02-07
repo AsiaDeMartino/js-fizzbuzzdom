9b9b7a0f023b3fe4a6ce134912e2c403e8b401ee
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-08 00:51:17 +0100

    Aggiunti casi Fizz e Buzz

--- myscript.js
+++ myscript.js
@@ -1,22 +1,28 @@
 // collegare variabile ad un contenitore in html e selezioniamo il container
-const container = document.getElementById("container_griglia");  
-console.log(container);
+const CONTAINER = document.getElementById("container_griglia");  
+console.log(CONTAINER);
 
 //creare loop che stampa numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    const quadrato = document.createElement("div");
-    container.append(quadrato);
-    quadrato.classList.add("quadrato");
+    const QUADRATO = document.createElement("div");
+    CONTAINER.append(QUADRATO);
+    QUADRATO.classList.add("quadrato");
 
     if (i % 3 == 0 && i % 5 == 0){                         //selezionare i numeri che sono sia multipli di 3 che multipli di 5
-        quadrato.classList.add("FizzBuzz");
+        QUADRATO.classList.add("FizzBuzz");
         const FIZZBUZZ = "FizzBuzz";
-        quadrato.append(FIZZBUZZ);
+        QUADRATO.append(FIZZBUZZ);
+    } else if (i % 3 == 0) {                                //selezionare multipli di 3
+        QUADRATO.classList.add("Fizz");
+        const FIZZ = "Fizz"
+        QUADRATO.append(FIZZ);
+    } else if (i % 5 == 0) {                                //selezionare multipli di 5
+        QUADRATO.classList.add("Buzz");
+        const BUZZ = "Buzz"
+        QUADRATO.append(BUZZ);
     }
 }       
         
-//selezionare multipli di 3
-//selezionare multipli di 5
 
 
 
