f2ca5de6017b536a0a3d25c6861623f5b50a5880
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-08 00:19:03 +0100

    creata griglia numeri

--- myscript.js
+++ myscript.js
@@ -1,5 +1,18 @@
-// collegare variabile ad un contenitore in html
+// collegare variabile ad un contenitore in html e selezioniamo il container
+const container = document.getElementById("container_griglia");  
+console.log(container);
+
 //creare loop che stampa numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    const quadrato = document.createElement("div");
+    container.append(quadrato);
+    quadrato.classList.add("quadrato");
+    quadrato.append(i);
+}       
+        
+
     //selezionare i numeri che sono sia multipli di 3 che multipli di 5
-        //selezionare multipli di 3
-        //selezionare multipli di 5
+    //selezionare multipli di 3
+    //selezionare multipli di 5
+
+
